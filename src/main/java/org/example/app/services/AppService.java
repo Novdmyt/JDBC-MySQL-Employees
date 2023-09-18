1f5232d5db1bc00924f2bbdb05e7548648a1fdca
Novdmyt <135864604+Novdmyt@users.noreply.github.com>
2023-09-18 16:05:29 +0300

    Add service

--- src/main/java/org/example/app/services/AppService.java
+++ src/main/java/org/example/app/services/AppService.java
@@ -1,10 +1,16 @@
 package org.example.app.services;
 
+import org.example.app.controllers.employee.EntityController;
 import org.example.app.controllers.positions.PositionController;
+import org.example.app.entities.Employee;
 import org.example.app.exceptions.OptionException;
+import org.example.app.services.employees.BaseService;
+import org.example.app.services.employees.EmployeeService;
 import org.example.app.services.positions.PositionService;
 import org.example.app.utils.AppStarter;
 import org.example.app.utils.Constants;
+import org.example.app.utils.StringFormatter;
+import org.example.app.views.employee.OptionView;
 import org.example.app.views.positions.PositionView;
 
 public class AppService {
@@ -17,8 +23,15 @@ public class AppService {
     }
 
     public void getEmployees() {
-        // TODO make Employees actions
-        System.out.println("Employees actions");
+        OptionView view = createView(Constants.TABLE_EMPLOYEES);
+        BaseService<Employee> service = new EmployeeService();
+        EntityController<Employee> controller = new EntityController<>(view, service);
+        controller.run();
+    }
+    private OptionView createView(String tableName) {
+        return new OptionView(StringFormatter.capitalize(tableName),
+                "Create a new "  + StringFormatter.removeLast(tableName),
+                "View " + tableName);
     }
 
     public void getNoSuchOption(int choice) {
