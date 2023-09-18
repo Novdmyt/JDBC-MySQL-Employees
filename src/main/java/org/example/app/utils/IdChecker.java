8e9b28a850b66132baf3a60f1e1867de144faf0e
Novdmyt <135864604+Novdmyt@users.noreply.github.com>
2023-09-18 14:49:34 +0300

    Add class Checker

---
+++ src/main/java/org/example/app/utils/IdChecker.java
@@ -0,0 +1,32 @@
+package org.example.app.utils;
+import org.example.app.database.DBConn;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+
+public class IdChecker {
+
+    public static boolean isIdValid(String id) {
+        return id.isEmpty() || !id.matches(Constants.ID_RGX);
+    }
+
+    public static boolean isIdExists(int id) {
+
+        try {
+            String query = "SELECT COUNT(id) FROM " + Constants.TABLE_POSITIONS + " WHERE id = ?";
+            PreparedStatement pst = DBConn.connect().prepareStatement(query);
+            pst.setInt(1, id);
+
+            try (ResultSet rs = pst.executeQuery()) {
+                if (rs.next()) {
+                    return rs.getBoolean(1);
+                }
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
+}
