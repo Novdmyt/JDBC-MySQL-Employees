564f22516642f8e2947640dc6c7d4472ea900a54
Novdmyt <135864604+Novdmyt@users.noreply.github.com>
2023-09-18 16:06:10 +0300

    Add method getData

--- src/main/java/org/example/app/views/employee/EmployeeCreateView.java
+++ src/main/java/org/example/app/views/employee/EmployeeCreateView.java
@@ -3,14 +3,15 @@ package org.example.app.views.employee;
 public class EmployeeCreateView extends CreateView {
     @Override
     public String[] getData() {
-        String positionId = getUserInput("Enter position Id: ");
+
         String firstName = getUserInput("Enter first name: ");
         String lastName = getUserInput("Enter last name: ");
         String birthDate = getUserInput("Enter birth date in format YYYY-MM-DD: ");
-        String phone = getUserInput("Enter phone in format xxx xxx-xxxx: ");
+        String positionId = getUserInput("Enter position Id: ");
+        String phone = getUserInput("Enter phone in format xxx xxx-xxx: ");
         String salary = getUserInput("Enter salary: ");
 
-        return new String[]{positionId, firstName, lastName, birthDate,  phone, salary};
+        return new String[]{firstName, lastName, birthDate, positionId, phone, salary};
     }
 }
 
